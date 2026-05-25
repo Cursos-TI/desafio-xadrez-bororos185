d57d4102e568b43af12e1ffb22263fc1898bb61d
bororos185 <joaovictorodrigues0701@gmail.com>
2026-05-25 20:32:50 +0000

    mestre

--- xadrez.c
+++ xadrez.c
@@ -1,6 +1,48 @@
-#include<stdio.h>
+#include <stdio.h>
+
+void moverTorre(int casas) {
+    if(casas == 0) {
+        return;
+    }
+
+    printf("Torre para a direita: %d\n", casas);
+    moverTorre(casas - 1);
+}
+
+void moverBispo(int casas) {
+    if(casas == 0) {
+        return;
+    }
+
+    printf("Bispo cima direita: %d\n", casas);
+    moverBispo(casas - 1);
+}
+
+void moverDama(int casas) {
+    if(casas == 0) {
+        return;
+    }
+
+    printf("Dama para a esquerda: %d\n", casas);
+    moverDama(casas - 1);
+}
+
+void moverCavalo(int movimentos) {
+    if(movimentos == 0) {
+        return;
+    }
+
+    printf("Cima\n");
+    printf("Cima\n");
+    printf("Direita\n");
+
+    moverCavalo(movimentos - 1);
+}
+
 int main() {
+
     int opcao;
+
     printf("==== Jogo de Xadrez ====\n");
     printf("Escolha a peca:\n");
     printf("1. Torre - Movimento em linha reta\n");
@@ -9,51 +51,45 @@ int main() {
     printf("4. Cavalo - Movimento em L\n");
     printf("5. Sair\n");
     printf("Opcao: ");
+
     scanf("%d", &opcao);
+
     switch(opcao) {
-//movimento da torre
-    case 1:
-    printf("Movendo a torre: \n");
-    for(int i =1; i <=5; i++){
-        printf("Torre para a direita: %d\n", i);
+
+        // Torre
+        case 1:
+            printf("Movendo a torre:\n");
+            moverTorre(5);
+            break;
+
+        // Bispo
+        case 2:
+            printf("Movendo o bispo:\n");
+            moverBispo(5);
+            break;
+
+        // Dama
+        case 3:
+            printf("Movendo a dama:\n");
+            moverDama(8);
+            break;
+
+        // Cavalo
+        case 4:
+            printf("Movendo o cavalo:\n");
+            moverCavalo(1);
+            break;
+
+        // Sair
+        case 5:
+            printf("Saindo do jogo...\n");
+            break;
+
+        // Inválido
+        default:
+            printf("Opcao invalida!\n");
+            break;
     }
-    break;
-//movimento do bispo
-    case 2:
-    printf("Movendo o bispo: \n");
-    for(int i =1; i <=5; i++){
-        printf("Cima Direita %d\n", i);
-    }
-    break;
-//movimento da dama
-    case 3:
-    printf("Movendo a dama: \n");
-    int casaDama = 1;
-    do {
-        printf("Dama para a esquerda: %d\n", opcao);
-        opcao++;
-    } while (opcao <= 8);
-    break;
-//movimento do cavalo
-    case 4:
-    int movimentoCavalo = 1;
-    while (movimentoCavalo--)
-    {
-      for(int i = 0; i < 2; i++) {
-        printf("Cima\n", i);
-      }
-      printf("Direita\n");
-    }
-    break;
-//sair do programa
-    case 5:
-    printf("Saindo do jogo...\n");
-    break;
-//opcao invalida
-    default:
-    printf("Opcao invalida. Tente novamente.\n");
-    break;
-   
-    }
-return 0;
+
+    return 0;
 }
